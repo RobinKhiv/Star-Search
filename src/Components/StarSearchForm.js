17b12bc63b2aae04385a006691f00fe86cc7fb8d
Robin Khiv <robinkhiv@gmail.com>
2019-04-07 15:27:03 -0700

    form able to get input

--- src/Components/StarSearchForm.js
+++ src/Components/StarSearchForm.js
@@ -1,17 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import StarSearchApiService from '../Services/StarSearchApiServices'
 
 export class StarSearchForm extends Component {
+  constructor(props){
+    super(props)
+        this.searchInput = React.createRef();
+  }
   handleSubmit = event => {
       event.preventDefault();
-      console.log('howdy')
+      const {starFilter, wookieLang } = event.target;
+      const isWookie= wookieLang.checked ? '&format=wookiee' : '';
+      const searchFilter = starFilter.value;
+      const searchValue = this.searchInput.current.value;
+      StarSearchApiService.get(searchFilter, isWookie, searchValue)
   }
   //planets, spaceships, vehicles, characters, films or species
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <label for='starFilter'>Search By: </label>
-        <select name='starFilter'>
-            <option value='null'></option>
+        <label htmlFor='starFilter'>Choose your base search: </label>
+        <select name='starFilter' required>
+            <option value=''></option>
             <option value='planets'>Planets</option>
             <option value='starships'>Spaceship</option>
             <option value='vehicles'>Vehicle</option>
@@ -19,10 +28,10 @@ export class StarSearchForm extends Component {
             <option value='films'>Film</option>
             <option value='species'>Species</option>
         </select>
-        <input type='checkbox' name='wookieLang' value='isWookie'/>
-        <label for='wookieLang'>Chewbacca is my family</label> <br/>
-        <label for='searchTextBox'>Search: </label>
-        <input type='text' name='searchTextbox' placeholder='chewbacca'></input>
+        <input type='checkbox' name='wookieLang' id='isWookie' value='iswookie'/>
+        <label htmlFor='wookieLang'>Chewbacca is my family</label> <br/>
+        <label htmlFor='searchTextBox'>Search: </label>
+        <input type='text' name='searchTextbox' id='searchTextbox' ref={this.searchInput} placeholder='chewbacca' required></input>
         <input type='submit' name='submit' value='Search The Stars'/>
       </form>
     )
